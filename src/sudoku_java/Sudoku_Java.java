91d7152f79216f989cf090ba1e7a15c993382ed0
Cristhian Valladolid <cmvc99@gmail.com>
2024-03-20 23:11:11 -0500

    Base para resolver sudoku
    Arreglo básico [9],para probar método de inserción y lectura, falta implementar lógica de resolución y luego hacer el array bidimensional de 9x9

--- src/sudoku_java/Sudoku_Java.java
+++ src/sudoku_java/Sudoku_Java.java
@@ -4,17 +4,37 @@
  */
 package sudoku_java;
 
+import java.util.Scanner;
+
 /**
  *
  * @author crist
  */
 public class Sudoku_Java {
 
-    /**
-     * @param args the command line arguments
-     */
+    static Scanner sc= new Scanner(System.in);
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Cual es tu nombre");
+        String nombre= sc.nextLine();
+        System.out.println("Hola "+ nombre + ".");
+        //Array basico para ir probando diseño y logica
+        int array[] = new int[9];
+        registrardatos(array);
+        imprimirdatos(array);
+        
+        
+        int[][]sudoku = new int[9][9]; //Matriz bidimensional para el sudoku
     }
-    
+    public static void registrardatos(int array[]){
+        System.out.println("Ingresa en orden los 9 numeros ");
+        for (int i = 0; i < array.length; i++) {
+            array[i]= sc.nextInt();
+        }
+    }
+    public static void imprimirdatos(int array[]){
+        System.out.println("Tus 9 números fueron: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " - " );
+        }
+    }
 }
