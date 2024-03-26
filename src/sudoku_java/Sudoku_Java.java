1823a9916f3ac8008aa98a97fc8a26e27bbd7a89
Cristhian Valladolid <cmvc99@gmail.com>
2024-03-26 11:02:19 -0500

    Multiples Cambios
    Se creo un metodo generico para leer la clase de tipo array Boolean e Integer, se craron dos metodos para resolver lineas por logica, falta mostrar resultados y se borro codigo redundante

--- src/sudoku_java/Sudoku_Java.java
+++ src/sudoku_java/Sudoku_Java.java
@@ -12,52 +12,94 @@ import java.util.Scanner;
  */
 public class Sudoku_Java {
 
-    static Scanner sc= new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Cual es tu nombre");
-        String nombre= sc.nextLine();
-        System.out.println("Hola "+ nombre + ".");
+        String nombre = sc.nextLine();
+        System.out.println("Hola " + nombre + ".");
         //Array basico para ir probando diseño y logica
         System.out.println("Los 0 ingresados representan espacio en blanco");
         int array[] = new int[9];
         registrardatos(array);
-        imprimirdatos(array);
-        
-        
-        int[][]sudoku = new int[9][9]; //Matriz bidimensional para el sudoku
+        imprimirDatos(array);
+        resolverLinea(array);
+
+        int[][] sudoku = new int[9][9]; //Matriz bidimensional para el sudoku
+
     }
-    public static void registrardatos(int array[]){
+
+    public static void registrardatos(int array[]) {
         System.out.println("Ingresa en orden los 9 numeros ");
         for (int i = 0; i < array.length; i++) {
-            array[i]= sc.nextInt();
+            array[i] = sc.nextInt();
         }
     }
-    public static void imprimirdatos(int array[]){
+
+    public static void imprimirDatos(int array[]) {
         System.out.println("Tus 9 números fueron: ");
         for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " - " );
+            System.out.print(array[i] + " - ");
         }
     }
-    public static void resolverLinea(int sudoku[]){
-        boolean[] numUsados= new boolean[10]; // Números del 0 al 9
-        System.out.println("Resolviendo ..........");
-        
-        for (int i = 0; i < sudoku.length; i++) { //for usado para llenar los numeros usados con true y false
-            if (sudoku[i]!=0) {
-                numUsados[sudoku[i]] = true;
-            }
-            
+
+    // Método genérico para imprimir un array de cualquier tipo de envoltura
+    public static <T> void imprimirDatosGenericos(T[] array) {
+        System.out.println("Tus datos son: ");
+        for (T elemento : array) {
+            System.out.print(elemento + " ");
         }
-        for (int i = 0; i < array.length; i++) {
-            if (array[i]==0) {
-                for (int j = 0; j < array.length; j++) {
-                    boolean name = array[i]== arrayCompleto[j];                            
-                }
-            }
-        }
-        
+        System.out.println();
     }
-         // Función para resolver la línea
+
+    public static void resolverLinea(int sudoku[]) {
+        //Metodo 1 
+        /*Este enfoque utiliza dos arrays: uno para almacenar los números proporcionados por el usuario y otro para los números posibles en cada celda. 
+        Se reemplazan los números no válidos con 0 en el array de números posibles. 
+        Luego, se comienza con una matriz de posibles números y se van restringiendo según las reglas del sudoku, hasta obtener una solución válida.*/
+
+        int[] arrayNumeros = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        for (int i = 0; i < arrayNumeros.length; i++) {
+            // Supongamos que la función esNumeroValido() verifica si el número es válido o no
+            if (!esNumeroValido(sudoku[i])) {
+                arrayNumeros[i] = 0; // Reemplazar el número no válido con 0
+            }
+        }
+        
+        imprimirDatos(arrayNumeros);
+        //Metodo2
+        /*En este enfoque se utiliza un solo array para almacenar la fila de números y un array booleano para marcar qué números han sido utilizados.
+        Se recorre la fila y se marcan los números ya proporcionados. Luego, para las celdas vacías, se asigna el primer número disponible que no ha sido utilizado. */
+
+//        Boolean[] numUsados= new Boolean[10]; // Números del 0 al 9
+//        System.out.println("Resolviendo ..........");
+//        
+//        for (int i = 0; i < sudoku.length; i++) { //for usado para llenar los numeros usados con true y false
+//            if (sudoku[i]!=0) {
+//                numUsados[sudoku[i]] = true;
+//            }
+//            
+//        }
+//        imprimirDatosGenericos(numUsados);
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i]==0) {
+//                for (int j = 0; j < array.length; j++) {
+//                    boolean name = array[i]== arrayCompleto[j];                            
+//                }
+//            }
+//        }
+    }
+
+    public static boolean esNumeroValido(int numero) {
+        // Verificar si el número está en el rango válido (del 1 al 9)
+        if (numero < 1 || numero > 9) {
+            return false;
+        }
+        return true;
+    }
+
+    // Función para resolver la línea
     public static void resolverLinea2(int[] numeros) {
         boolean[] usados = new boolean[10]; // Marcador para números usados
 
@@ -79,26 +121,5 @@ public class Sudoku_Java {
             }
         }
     }
-    
-    public static void resolverLinea3(int[] numeros) {
-        boolean[] usados = new boolean[10]; // Marcador para números usados
 
-        for (int i = 0; i < 9; i++) {
-            if (numeros[i] != 0) {
-                usados[numeros[i]] = true;
-            }
-        }
-
-        for (int i = 0; i < 9; i++) {
-            if (numeros[i] == 0) {
-                for (int num = 1; num <= 9; num++) {
-                    if (!usados[num]) {
-                        numeros[i] = num;
-                        usados[num] = true;
-                        break;
-                    }
-                }
-            }
-        }
-    }
 }
